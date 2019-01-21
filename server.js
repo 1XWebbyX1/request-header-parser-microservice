4b28884d584680f26083ff57b542d53bec41d5df
Khayati Asrani <devinc@TheDen-3.local>
2019-01-21 22:25:31 +0530

    handle 404 error

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ app.get("/api/whoami", function (req, res) {
   res.json({ipaddress : req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
 });
 
-
+// 404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+})
 
 // listen for requests :)
 var listener = app.listen(PORT, function () {
