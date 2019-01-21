9be07ccfcf48ed96bb4d0f3a80d7f9d50806ad83
Khayati Asrani <devinc@TheDen-3.local>
2019-01-21 22:08:10 +0530

    populate with files

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+//mount static assests
+app.use(express.static('src'));
+
+//basic html-routing
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+//api routing
+app.get("/api/whoami", function (req, res) {
+  res.json({ipaddress : req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
